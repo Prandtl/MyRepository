51deb9a858b52288143e26c42de1f029deccdf2c
VUrdalak211 <vurdalak211@gmail.com>
2014-02-19 14:12:35 +0600

    changes

--- TicTacToe/1-TicTacToe/Program.cs
+++ TicTacToe/1-TicTacToe/Program.cs
@@ -8,64 +8,88 @@ namespace TicTacToe
 {
     class Program
     {
-        static void Swap(int[] array,int x1, int x2)
-        {
-            int t = array[x2];
-            array[x2] = array[x1];
-            array[x1] = t;
-        }
+        public static HashSet<int[]> ended;
 
-        static int[] GenerateArray(int N)
+        public static int[][] combinations = new int[][] {new int[]{ 1, 2, 3 }, 
+                                                new int[]{ 4, 5, 6 },
+                                                new int[]{ 7, 8, 9 },
+                                                new int[]{ 1, 4, 7 },
+                                                new int[]{ 2, 5, 8 },
+                                                new int[]{ 3, 6, 9 },
+                                                new int[]{ 1, 5, 9 },
+                                                new int[]{ 3, 5, 7 }};
+
+        static void Swap(ref int x1, ref int x2)
         {
-            return Enumerable.Range(1, N)
-                             .ToArray();
+            int t = x1;
+            x2 = x1;
+            x2 = t;
         }
 
         static IEnumerable<int[]> GetPermutations(int N)
         {
-            int[] permutation = GenerateArray(N);
+            int[] permutation = Enumerable.Range(1, N).ToArray();
             while (true)
             {
                 yield return permutation;
-                int j = 0;
+                int j;
                 for (j = N - 2; j >= 0; j--)
                 {
                     if (permutation[j] < permutation[j + 1]) break;
                     if (j == 0) yield break;
                 }
-
-                int min = 0;
-                int minIndex = 0;
-                for (int i = j+1; i < N; i++)
-                {
-                    if ((permutation[i] < min && permutation[i] > permutation[j]) || (min == 0))
-                    {
-                        min = permutation[i];
-                        minIndex = i;
-                    }
-                }
-                Swap(permutation, j, minIndex);
-                var copy = permutation.Reverse().ToArray();
-                int t = 0;
+                int minIndex = j + 1;
                 for (int i = j + 1; i < N; i++)
                 {
+                    if (permutation[i] < permutation[minIndex] && permutation[i] > permutation[j])
+                        minIndex = i;
+                }
+                Swap(ref permutation[j], ref permutation[minIndex]);
+                var copy = permutation.Reverse().ToArray();
+                int t = 0;
+                for (int i = j + 1; i < N; i++, t++)
+                {
                     permutation[i] = copy[t];
-                    t++;
                 }
             }
+        }
+
+        static bool Includes(int[] first,int[] second)
+        {
+            foreach (var e in second)
+            {
+                if (!first.Contains(e)) return false;
+            }
+            return true;
+        }
+
+        static int[] FindCombination(int[] permutation)
+        {
+            foreach (var combination in Program.combinations)
+            {
+                if (Includes(permutation,combination))
+
+            }
+        }
+
+        static bool BeenBefore(int[] permutation)
+        {
+            for (int i=5;i<=9;i++)
+            {
+                int[]subSet=permutation.Where((x,j)=>j<i).ToArray();
+                if (Program.ended.Contains(subSet))
+                    return true;
+            }
+            return false;
         }
 
         static void Main()
         {
-           foreach (var e in GetPermutations(4))
-            {
-                foreach (var k in e)
-                    Console.Write(k + " ");
-                Console.WriteLine();
-            
-            }
+           foreach( var permutation in GetPermutations(9))
+           {
+               if (BeenBefore(permutation)) continue;
+               FindCombination(permutation);
+           }
         }
-
-        
     }
 }

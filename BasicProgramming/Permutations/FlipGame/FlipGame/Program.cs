f404b45705a6bbf6c5a105016cc15b56be1c8100
VUrdalak211 <vurdalak211@gmail.com>
2014-02-26 00:05:46 +0600

    Permutations added

---
+++ BasicProgramming/Permutations/FlipGame/FlipGame/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipGame
+{
+    class Program
+    {
+        static bool IsTheWin(int[] field)
+        {
+            for (int i = 1; i < field.Length; i++)
+                if (field[i] != field[i - 1]) return false;
+            return true;
+        }
+
+        static void Flip(ref int piece)
+        {
+            if (piece == 1)
+            {
+                piece = 0;
+                return;
+            }
+            piece = 1;
+            return;
+        }
+
+        static void FlipPieces(int[] field, int pointx, int pointy)
+        {
+            int point = pointx - 1 + 4 * (pointy - 1);
+            Flip(ref field[point]);
+            if (point - 4 >= 0)
+                Flip(ref field[point - 4]);
+            if (point + 4 < 16)
+                Flip(ref field[point + 4]);
+            if (point % 4 != 0)
+                Flip(ref field[point - 1]);
+            if (point % 4 != 3)
+                Flip(ref field[point + 1]);
+        }
+
+        static void Main(string[] args)
+        {
+            StringBuilder input = new StringBuilder();
+            for (int i = 0; i < 4; i++)
+                input.Append(Console.ReadLine());
+            int[] field = input.ToString().Select(x => (x == 'b') ? 0 : 1).ToArray();
+            for (int i = 0; i < field.Length; i++)
+            {
+                if (i % 4 == 0) Console.WriteLine();
+                Console.Write(field[i]);
+            }
+
+            FlipPieces(field, 1, 3);
+
+            for (int i = 0; i < field.Length; i++)
+            {
+                if (i % 4 == 0) Console.WriteLine();
+                Console.Write(field[i]);
+            }
+
+            
+        }
+    }
+}
---
+++ BasicProgramming/Permutations/Stones/Stones/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Stones
+{
+    class Program
+    {
+        static IEnumerable<bool[]> Subsets(int N)
+        {
+            var max = 1 << (N);
+            for (long number = 0; number < max; number++)
+            {
+                var mask = 1;
+                var subset = new bool[N];
+                for (int j = N - 1; j >= 0; j--)
+                {
+                    subset[j] = (number & mask) != 0;
+                    mask <<= 1;
+                }
+                yield return subset.ToArray();
+
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int[] weight = input.Split(' ').Select(x => int.Parse(x)).ToArray();
+            int sum = weight.Sum();
+            int minDifference = sum;
+            foreach (var e in Subsets(weight.Length))
+            {
+                int subsetSum = 0;
+                int t = 0;
+                foreach (var c in e)
+                {
+                    if (c) subsetSum += weight[t];
+                    t++;
+                }
+
+                if (Math.Abs(sum - 2 * subsetSum) < minDifference) minDifference = Math.Abs(sum - 2 * subsetSum);
+
+            }
+            Console.WriteLine(minDifference);
+        }
+    }
+}

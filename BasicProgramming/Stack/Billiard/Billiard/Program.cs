839b8f4d71b8d72ed5fa743e7af86df6832eca9d
VUrdalak211 <vurdalak211@gmail.com>
2014-03-03 23:00:18 +0600

    Stacks added -Billiard added

---
+++ BasicProgramming/Stack/Billiard/Billiard/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billiard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            int n = int.Parse(Console.ReadLine());
+            var input = new int[n];
+            for (int i = 0; i < n; i++)
+                input[i] = int.Parse(Console.ReadLine());
+            var ballsOut = new Stack<int>(input.Reverse());
+            var firstBall = ballsOut.Pop();
+            var inside = new Stack<int>(Enumerable.Range(1, firstBall - 1));
+            var onTable = new Stack<int>(Enumerable.Range(firstBall + 1, n - firstBall).Reverse());
+            while (ballsOut.Count != 0)
+            {
+                if (inside.Count == 0)
+                {
+                    if (onTable.Count != 0)
+                    {
+                        inside.Push(onTable.Pop());
+                    }
+                    else
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                var neededBall = ballsOut.Pop();
+                if (inside.Peek() > neededBall)
+                {
+                    flag = false;
+                    break;
+                }
+                while (inside.Peek() < neededBall)
+                {
+                    if (onTable.Count != 0)
+                    {
+                        inside.Push(onTable.Pop());
+                    }
+                    else
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (!flag) break;
+                if (inside.Peek() == neededBall)
+                {
+                    inside.Pop();
+                    continue;
+                }
+            }
+            Console.WriteLine(flag ? "Not a proof" : "Cheater");
+        }
+    }
+}

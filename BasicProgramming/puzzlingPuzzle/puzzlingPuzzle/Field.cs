a7efb1149dd42483c22af0b69e4f60ed0e5d39c4
VUrdalak211 <vurdalak211@gmail.com>
2014-04-21 22:57:48 +0600

    puzzling puzzle

---
+++ BasicProgramming/puzzlingPuzzle/puzzlingPuzzle/Field.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace puzzlingPuzzle
+{
+    public static class ExtensionMethods
+    {
+        public static GameField.Point Add(this GameField.Point a, GameField.Point b)
+        {
+            return new GameField.Point(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static void Swap(this GameField.Tile a, GameField.Tile b)
+        {
+            var t = new GameField.Point(a.position.X, a.position.Y);
+            a.position = b.position;
+            b.position = t;
+        }
+    }
+
+    public class GameField
+    {
+        public Tile[] Field;
+        public Tile zeroTile;
+
+        public GameField()
+        {
+            Field = new Tile[]{new Tile(0,0," "),
+                               new Tile(1,0,"1"),
+                               new Tile(2,0,"2"),
+                               new Tile(0,1,"3"),
+                               new Tile(1,1,"4"),
+                               new Tile(2,1,"5"),
+                               new Tile(0,2,"6"),
+                               new Tile(1,2,"7"),
+                               new Tile(2,2,"8")};
+            zeroTile=Field[0];
+        }
+
+        public Tile[] GetNeighbours(Tile tile)
+        {
+            var a = new Point[]{new Point(-1,0),
+                                new Point(1,0),
+                                new Point(0,1),
+                                new Point(0,-1)}.Select(x => x.Add(tile.position))
+                                                .Where(x => (x.X >= 0 && x.X < 3) && (x.Y >= 0 && x.Y < 3));
+            return Field.Where(x => a.Contains(x.position)).ToArray();
+        }
+
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Point(int x,int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            
+        }
+        public class Tile
+        {
+            public Point position { get; set; }
+            public string value;
+
+            public Tile(int x,int y, string val)
+            {
+                position = new Point(x, y);
+                value = val;
+            }
+        }
+
+    }
+}
---
+++ BasicProgramming/puzzlingPuzzle/puzzlingPuzzle/Game.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzlingPuzzle
+{
+    public partial class Game : Form
+    {
+        GameField field;
+        public Timer timer;
+
+        public Game()
+        {
+            this.ClientSize = new Size(64 * 3 + 128, 64 * 3);
+            this.Text = "8-puzzle, nigga!";
+            ResizeRedraw = true;
+            DoubleBuffered = true;
+            KeyPreview = true;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            StartGame();
+        }
+
+        void StartGame()
+        {
+            field = new GameField();
+
+            this.Paint += Game_Paint;
+
+            timer = new Timer();
+            timer.Interval = 10;
+            timer.Tick += timer_Tick;
+            timer.Start();
+            this.KeyDown += new KeyEventHandler(Game_KeyDown);
+            
+        }
+
+        void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            var neighbours = field.GetNeighbours(field.zeroTile);
+            var a=neighbours.Where(x=>x.position==field.zeroTile.position.Add(keyVector[e.KeyCode]));
+            if (a.Count() != 0)
+            {
+                var movingTile=a.First();
+                movingTile.Swap(field.zeroTile);
+            }
+                
+        }
+
+        Dictionary<Keys, GameField.Point> keyVector = new Dictionary<Keys, GameField.Point>{{Keys.Down,new GameField.Point(0,-1)},
+                                                                                            {Keys.Up,new GameField.Point(0,1)},
+                                                                                            {Keys.Left,new GameField.Point(1,0)},
+                                                                                            {Keys.Right,new GameField.Point(-1,0)}}; 
+
+        //void Game_KeyPress(object sender, KeyPressEventArgs e)
+        //{
+            
+            
+        //}
+
+        void Game_Paint(object sender, PaintEventArgs e)
+        {
+            Font drawFont = new Font("Arial", 32);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            var graphics=e.Graphics;
+            graphics.FillRectangle(Brushes.Aqua,0,0,192,192);
+            foreach (var tile in field.Field)
+            {
+                if (tile.value != " ")
+                {
+                    graphics.FillRectangle(Brushes.BurlyWood, tile.position.X * 64 + 1, tile.position.Y * 64 + 1, 62, 62);
+                    graphics.DrawString(tile.value, drawFont, drawBrush, new PointF(tile.position.X * 64 + 12, tile.position.Y * 64+5));
+                }
+            }
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}

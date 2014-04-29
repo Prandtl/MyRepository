6256eb33185048a464e413281ab7abf7b9f44d7b
VUrdalak211 <vurdalak211@gmail.com>
2014-04-29 23:48:05 +0600

    giantGraph

---
+++ BasicProgramming/GiantGraph/GiantGraph/Graph.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace GiantGraph
+{
+    public partial class Graph : Form
+    {
+        public Graph()
+        {
+            var chart = new Chart()
+            {
+                ChartAreas = { new ChartArea() },
+            };
+            chart.Dock = DockStyle.Fill;
+            Controls.Add(chart);
+
+            var graph = new RandomGraph();
+            for (int i = 0; i < 10000; i++)
+            {
+                graph.AddVertice(2);
+            }
+            
+
+            var series = new Series();
+            for (int i = 0; i < graph.graph.Count;i++)
+                series.Points.Add(new DataPoint(i, graph.graph[i].Count));
+            series.ChartType = SeriesChartType.FastLine;
+
+            chart.Series.Add(series);
+        }
+    }
+
+    class RandomGraph
+    {
+        public List<List<int>> graph;
+
+        int degreesSum;
+
+        Random rand = new Random();
+
+        public RandomGraph()
+        {
+            graph = new List<List<int>>();
+            graph.Add(new List<int>() { 1 });
+            graph.Add(new List<int>() { 0 });
+            degreesSum = 2;
+        }
+
+        public void AddVertice(int M)
+        {
+            graph.Add(new List<int>());
+            for (int k = 0; k < M; k++)
+            {
+                var a = rand.Next(0, degreesSum);
+                int i = 0;
+                while (a > 0)
+                {
+                    a -= graph[i].Count;
+                    i++;
+                }
+                graph[i].Add(graph.Count - 1);
+                graph.Last().Add(i);
+                degreesSum += 2;
+            }
+        }
+
+    }
+}
--- Scripts/Huffman/Huffman.js
+++ Scripts/Huffman/Huffman.js
@@ -1,32 +1,30 @@
 
-function node(name,fr,used,ptr,code)
+function node(name,fr,used,link,code)
 {
 	this.name=name;
 	this.fr=fr;
 	this.used=used;
-	this.ptr=ptr;
+	this.link=link;
 	this.code=code;
 }
 
 
 tree=new Array();
-str = 'abracadabra';
+str = WSH.StdIn.ReadLine();
 alph=new Array();
 for(i=0;i<str.length;i++)
-alph[str.charAt(i)]=0
+	alph[str.charAt(i)]=0
 for(i=0;i<str.length;i++)
-alph[str.charAt(i)]++
+	alph[str.charAt(i)]++
 for(i in alph)
 {
 	n=new node(i,alph[i],0,null,'');
 	tree.push(n);
 }
 
-for(i=0;i<tree.length;i++)
-	WSH.echo(tree[i].name,' ',tree[i].fr,' ',tree[i].used);
-WSH.echo('===================');
 	
 length=tree.length;
+
 for(j=0;j<length-1;j++)
 {
 	fr1 = str.length;
@@ -39,10 +37,9 @@ for(j=0;j<length-1;j++)
 			num1 = i;
 		}
 	}
-	WSH.echo(tree[num1].name+' '+tree[num1].used);
 	tree[num1].used = 1;
 	tree[num1].code = 0;
-	tree[num1].link = tree.length-1;
+	tree[num1].link = tree.length;
 	fr2 = str.length;
 	num2 = 0;
 	for(i=0;i<tree.length;i++)
@@ -53,17 +50,14 @@ for(j=0;j<length-1;j++)
 			num2 = i;
 		}
 	}
-	WSH.echo(tree[num2].name+' '+tree[num2].used);
 	tree[num2].used = 1;
 	tree[num2].code = 1;
-	tree[num2].link = tree.length-1;
+	tree[num2].link = tree.length;
 	tree.push(new node(tree[num1].name + tree[num2].name, tree[num1].fr + tree[num2].fr, 0, null, ''));
 
 }
-for(i=0;i<tree.length;i++)
-	WSH.echo(tree[i].name,' ',tree[i].fr,' ',tree[i].used)
-	
 
+	
 
 code = new Array();
 for(i=0;i<length;i++)
@@ -74,8 +68,6 @@ for(i=0;i<length;i++)
 	{
 		code[tree[i].name] = tree[j].code + code[tree[i].name];
 		j = tree[j].link;
-		WSH.echo(j);
-		break;
 	}
 }
 

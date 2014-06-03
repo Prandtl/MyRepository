987432c6922c3613c6c82ff81b97dda4e2a9ac8d
VUrdalak211 <vurdalak211@gmail.com>
2014-06-03 12:24:25 +0600

    Js - upd

--- Scripts/Boyer Moore/BM.js
+++ Scripts/Boyer Moore/BM.js
@@ -1,101 +1,26 @@
-function PreBMbc(a){
-	table=new Array()
-	WSH.echo('BC starts')
-	for (i=0;i<a.length;i++)
+s='abaxabaz'
+t='aba'
+n=s.length;
+m=t.length;
+bmBC=new Array()
+for(j=0;j<m;j++)
+	bmBC[t.charAt(j)]=j+1
+i=0;
+while(i!=n){
+	j=m-1;
+	l=0;
+	while(s.charAt(i+j)==t.charAt(j) && t.charAt(j)!='')
 	{
-		table[a.charAt(i)]=a.length-1-i;
-		WSH.echo(a.charAt(i),table[a.charAt(i)])
+		l++;
+		j--;
 	}
-	
-	WSH.echo('BC is done: ')
-	for (i in table)
-		WSH.echo(i,table[i])
-	return table
+	if(j<0)
+	{
+		WSH.Echo(i);
+		i+=1;
+	}
+	else
+	{
+		i+=m-l- bmBC[t.charAt(j)]
+	}
 }
-
-function IsPrefix(x,p){
-	j=0
-	for(q=p;q<x.length;q++)
-	{
-		if(x.charAt(q)!=x.charAt(j))
-			return false
-	}
-	return true
-}
-
-function SuffixLength(x,p){
-	l=0
-	i=p
-	j=x.length-1
-	while(i>=0&&x.charAt(i)==x.charAt(j))
-	{
-		l++
-		i--
-		j--
-	}
-	return l
-} 	
-
-function PreBMgs(x){
-	WSH.echo('GS starts')
-	
-	m=x.length
-	table=new Array()
-	lastPrefixPosition=m
-	for(i=m-1;i>=0;i-=1)
-	{	
-		if(IsPrefix(x,i+1))
-		{
-			lastPrefixPosition=i+1
-		}
-		table[m-1-i]=lastPrefixPosition-i+m-1
-	}
-	for(i=0;i<m-1;i++)
-	{
-		slen=SuffixLength(x,i)
-		table[slen]=m-1-i+slen
-	}
-	WSH.echo('GS is done:')
-	for (i in table)
-		WSH.echo(i,table[i])
-	return table
-}	
-
-function BM(y,x){
-	res= new Array()
-	
-	n=y.length
-	m=x.length
-	if(m==0)
-		return res
-	
-	WSH.echo('preBM starts')
-	bmBC=PreBMbc(x)
-	bmGS=PreBMgs(x)
-	WSH.echo('preBM ended successfully')
-	WSH.echo()
-	
-	for(i=m-1;i<n;i++)
-	{
-		j=m-1
-		while(x.charAt(j)==x.charAt(i)&&(i>=0&&j>=0))
-		{
-			WSH.echo('i: ',i,'; ',x.charAt(i))
-			WSH.echo('j: ',j,'; ',x.charAt(j))
-			WSH.echo('-------------------------')
-			if (j==0)
-				res.push(i)
-			i--
-			j--
-		}
-		if (bmBC[y.charAt(i)]==undefined)
-			bcShift=m
-		else
-			bcShift=bmBC[y.charAt(i)]
-		i+=Math.max(bmGS[m-1-j],bcShift)
-	}
-	
-	return res
-}
-
-WSH.echo(BM('abeccaabadbabbad','abbad'))
--- Scripts/automat/a.js
+++ Scripts/automat/a.js
@@ -1,12 +1,19 @@
-t=WScript.StdIn.ReadLine()
-//t='abaxabaz'
+//t=WScript.StdIn.ReadLine()
+//s=WScript.StdIn.ReadLine()
+
+s='anananaananas'
+t='ananas'
+
 m=t.length
+
 alph=new Array()
 for(i=0;i<m;i++)
 	alph[t.charAt(i)]=0
+
 del=new Array(m+1)
 for(j=0;j<=m;j++)
 	del[j]=new Array()
+
 for(i in alph)
 	del[0][i]=0
 for(j=0;j<m;j++){
@@ -15,9 +22,25 @@ for(j=0;j<m;j++){
 	for(i in alph)
 		del[j+1][i]=del[prev][i]
 }
+
 for(j=0;j<=m;j++){
 	out=''
+	out+=j+': '
 	for(i in alph)
 		out+=del[j][i]+' '
 	WScript.Echo(out)
 }
+WSH.echo('------------------------------------')
+k=0
+q=del[0][s.charAt(0)]
+res=new Array()
+while(k<s.length)
+{	
+	WSH.echo(q,k,del[q][s.charAt(k)],s.charAt(k))
+	q=del[q][s.charAt(k)]
+	k++
+	if(q==m)
+		res.push(k-m+1)
+}
+WSH.echo('-------------------------------------')
+WSH.echo(res)
